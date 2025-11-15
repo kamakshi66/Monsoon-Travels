df73fc52ff2892938b2bcc0e989c3011468f1620
kamakshi66 <kamakshikanchipalli@gmail.com>
2025-11-15 18:58:59 +0530

    Update settings.py for Render and add dj-database-url to requirements

--- Backend/travelss/travelss/settings.py
+++ Backend/travelss/travelss/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 
 
 from pathlib import Path
@@ -107,11 +108,12 @@ WSGI_APPLICATION = 'travelss.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
