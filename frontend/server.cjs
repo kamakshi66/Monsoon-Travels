723bad73630071e1db7342b4e3481e3f34ef2ac2
kamakshi66 <kamakshikanchipalli@gmail.com>
2025-11-16 00:50:53 +0530

    Frontend ready for Render deployment

--- frontend/server.cjs
+++ frontend/server.cjs
@@ -1,8 +1,5 @@
 
 /* eslint-env node */
-
-
-/* eslint-env node */
 const express = require('express');
 const path = require('path');
 
@@ -12,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 // Serve static files from the Vite build folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// React Router support: send index.html for all other routes
+// React Router support: catch-all route for unknown URLs
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
